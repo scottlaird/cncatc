909f4d25250081bca9107f8142e7a12a43ebb2cd
Scott Laird <scott@sigkill.org>
2019-02-16 21:20:17 -0800

    Initial cleanup, replacing boilerplate and test code with something that will eventually be useful.  Beginnings of a speed-control state machine.  More comments.

--- CncAtc.ino
+++ CncAtc.ino
@@ -1,12 +1,34 @@
 /*
+
+  Control code for driving a AMB/Kress FME-W DI Tool-changing spindle from GRBL.
+
+  This is intended to run on an Arduino Mega that is wired up between
+  the GRBL controller and the spindle.  It reads spindle speed,
+  spindle direction, and coolant outputs from GRBL and outputs a
+  number of distinct controls:
+
+    - Spindle relay control (for powering the spindle down entirely).
+    - Spindle speed control (0-5v PWM; will need converted to 0-10v.
+      The AMB spindle reads 0v as 5k RPM and 10v as 25k RPM, while
+      GRBL outputs 0v as off, 0.1v as minimum RPM and 5v as maximum
+      RPM.
+    - 3x pneumatic solenoid controls.  2 for driving the tool changer
+      1 for use as an air assist/chip blower.
   
+  In addition, it reads from several ACS712-based current meters,
+  which are used to track the amount of current used by various parts
+  of the control circuitry of the system.
+
+  Finally, it drives a display, currently planned to be a 2.7" OLED
+  from Adafruit via SPI.  The display will show the current RPM,
+  current readings, and tool change status.
 
 */
 
 // include the library code:
 #include <LiquidCrystal.h>
 
-// Input defs
+// I/O pin definitions
 const int pinDcAmps=A0;
 const int pinSpindleAmps=A1;
 const int pinVacAmps=A2;
@@ -20,14 +42,37 @@ const int pinAirA=5;
 const int pinAirB=6;
 const int pinAirC=7;
 
+// RPM metrics.  v5000 is the value read on pinCncSpindleSpeed when
+// GRBL is sending 5,000 RPM.  v25000 is the value read on
+// pinCncSpindleSpeed when GRBL is sending 25,000 RPM.  Finally,
+// anything under v_off is considered to be 'off'.
+//
+// There really needs to be a state machine here that limits
+// transitions in and out of 'off' and blocks the spindle from
+// spinning while the ATC is in operation.  Also, spindle speed is
+// currently tracked using a decaying average, so we can average
+// readings across several cycles.  The produced RPM needs to be a bit
+// cleverer than this; it needs to stay fixed if the input voltage is
+// close to the existing setpoint, but then skew faster once it's
+// clearly out of bounds.  TODO(laird): review literature.
+const double v5000=521.8;
+const double v25000=600.5;
+const double v_off=512;
+
+
+// Spindle States
+const int stateSpindleOff=0;  // Can go to SpeedChanging or ToolChangeStart
+const int stateSpindleSpeedChanging=1; // Can go to Off or Steady
+const int stateSpindleSpeedSteady=2; // Can go to Changing
+const int stateSpindleToolChangeStart=3; // Can go to ToolChanging
+const int stateSpindleToolChanging=4; // Can go to ToolChangeWait
+const int stateSpindleToolChangeWait=5; // Can go to Off
+int stateSpindle = stateSpindleOff;
 
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
 LiquidCrystal lcd(38,39,40,41,42,43);
-
-long totalVolts;
-int countVolts;
 
 // Calculate amps given a raw analog reading from an ACS712 current
 // measurement IC.  These come in a few different sizes (5, 20, 30
@@ -42,10 +87,13 @@ int countVolts;
 // long int operations take ~4.5x as long as int operations.  So, this
 // would be faster if we did it all with long ints, but it's only a 2x
 // perf difference, and it'd take a lot longer to debug and deal with
-// overflow cases.
+// overflow cases.  So: double for now, and it can move to longs if
+// there's a demonstrable performance problem.
+//
+// Note: float==double on Arduino.
 double rawToAmps(int raw, int acs712_amps) {
   int mVperAmp = 185;
-  int ACSoffset = 2582;  // Averaged across several devices; should be 2500.
+  int ACSoffset = 2582;  // Averaged across several devices; supposed be 2500.
   double voltage;
   double amps;
 
@@ -62,17 +110,8 @@ double rawToAmps(int raw, int acs712_amps) {
     /* No good way to do error handling here... */
     break;
   }
-  //Serial.print(" r=");
-  //Serial.print(raw);
   voltage = raw / 1024.0 * 5000;
-  //Serial.print(" v=");
-  //Serial.print(voltage);
   amps = (voltage - ACSoffset) / mVperAmp;
-  //Serial.print(" a=");
-  //Serial.println(amps);
-
-  //countVolts++;
-  //totalVolts+=voltage;
   
   return amps;
 }
@@ -105,9 +144,6 @@ double decayingAverage(double a, double b, double fraction) {
 // nearest 1k RPM, which might be best.  Or maybe not.  Maybe just
 // for displaying?
 
-const double v5000=521.3;
-const double v25000=590.8;
-const double v_off=512;
 
 int analogToRpm(double v){
   if (v < v_off)
@@ -119,14 +155,13 @@ int analogToRpm(double v){
 void setup() {
   // Set up logging over USB
   Serial.begin(115200);
+  Serial.println(">>> booting");
 
   // Set up pins
   pinMode(pinSpindleOnOff, OUTPUT);
   pinMode(pinAirA, OUTPUT);
   pinMode(pinAirB, OUTPUT);
   pinMode(pinAirC, OUTPUT);
-
-  //pinMode(pinCncCoolant, INPUT_PULLUP);
 
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
@@ -136,29 +171,116 @@ void setup() {
 double dcAmps, spindleAmps, vacAmps, compressorAmps;
 double avgSpindleSpeed;
 
-void loop() {
-  //int spindleEnabled, spindleSpeed;
-  int rpm;
-
-  int cncCoolant, cncSpindleDirection, cncSpindleSpeed;
+void readAmps() {
   double new_dcAmps, new_spindleAmps, new_vacAmps, new_compressorAmps;
-  int airState = 0; // (millis()/10000)%2;  // Alternate 0/1 every 10s.
 
+  // Read current meters
   new_dcAmps = rawToAmps(analogRead(pinDcAmps), 20);
   new_spindleAmps = rawToAmps(analogRead(pinSpindleAmps), 5);
   new_vacAmps = rawToAmps(analogRead(pinVacAmps), 20);
   new_compressorAmps = rawToAmps(analogRead(pinCompressorAmps), 20);
 
-  
+  dcAmps = decayingAverage(new_dcAmps, dcAmps, 0.2);
+  spindleAmps = decayingAverage(new_spindleAmps, spindleAmps, 0.2);
+  vacAmps = decayingAverage(new_vacAmps, vacAmps, 0.2);
+  compressorAmps = decayingAverage(new_compressorAmps, compressorAmps, 0.2);
+  Serial.print(">>> current dc=");
+  Serial.print(dcAmps);
+  Serial.print(" spindle=");
+  Serial.print(spindleAmps);
+  Serial.print(" vac=");
+  Serial.print(vacAmps);
+  Serial.print(" compressor=");
+  Serial.println(compressorAmps);
+}
+
+int cncCoolant, cncSpindleDirection, cncSpindleSpeed;
+void readGrbl() {
   cncSpindleSpeed=analogRead(pinCncSpindleSpeed);
   cncSpindleDirection=digitalRead(pinCncSpindleDirection);
   cncCoolant=digitalRead(pinCncCoolant);
   avgSpindleSpeed=decayingAverage(cncSpindleSpeed, avgSpindleSpeed, 0.2);
 
-  dcAmps = decayingAverage(new_dcAmps, dcAmps, 0.2);
-  spindleAmps = decayingAverage(new_spindleAmps, spindleAmps, 0.2);
-  vacAmps = decayingAverage(new_vacAmps, vacAmps, 0.2);
-  compressorAmps = decayingAverage(new_compressorAmps, compressorAmps, 0.2);
+  Serial.print(">>> grbl cool=");
+  Serial.print(cncCoolant);
+  Serial.print(" dir=");
+  Serial.print(cncSpindleDirection);
+  Serial.print(" rawSpeed=");
+  Serial.print(cncSpindleSpeed);
+  Serial.print(" avgSpeed=");
+  Serial.println(avgSpindleSpeed);
+}
+
+long doneToolChangeStart, doneToolChangeWait;
+
+void loop() {
+  int rpm;
+  int wantToolChange;
+
+  // Generic serial logging format: start with '>>> ', then a word
+  // that describes which type of logging, then 0 or more name=value
+  // pairs, followed by a newline.
+  Serial.print(">>> starting ms=");
+  Serial.print(millis());
+  Serial.print(" statespindle=");
+  Serial.println(stateSpindle);
+
+  readAmps();
+  readGrbl();
+
+  wantToolChange=cncCoolant==0 && cncSpindleDirection==1;
+
+  rpm = analogToRpm(avgSpindleSpeed);
+
+  switch (stateSpindle) {
+  case stateSpindleOff:
+    if (wantToolChange) {
+      Serial.println(">>> spindlestate old=off new=toolchangestart");
+      stateSpindle=stateSpindleToolChangeStart;
+      doneToolChangeStart = millis() + 5000; // 5 second wait to make sure the spindle has spun down completely.
+    } else if (rpm > 0) {
+      Serial.println(">>> spindlestate old=off new=speedchanging");
+      stateSpindle=stateSpindleSpeedChanging;
+    }
+    break;
+  case stateSpindleSpeedChanging:
+    if (rpm > 0) {
+      Serial.println(">>> spindlestate old=speedchanging new=speedsteady");
+      stateSpindle=stateSpindleSpeedSteady;
+    } else {
+      Serial.println(">>> spindlestate old=speedchanging new=off");
+      stateSpindle=stateSpindleOff;
+    }
+    break;
+  case stateSpindleSpeedSteady: 
+    Serial.println(">>> spindlestate old=speedsteady new=speedchanging");
+    stateSpindle=stateSpindleSpeedChanging;
+    break;
+  case stateSpindleToolChangeStart:
+    if (millis() > doneToolChangeStart) {
+      Serial.println(">>> spindlestate old=toolchangestart new=toolchanging");
+      stateSpindle=stateSpindleToolChanging;
+    }
+    break;
+  case stateSpindleToolChanging:
+    digitalWrite(pinAirA, 1);  // Unlock
+    digitalWrite(pinAirB, 1);  // TODO(laird): pulse 500ms on, 500ms off, 500ms on, 500ms off
+    
+    if (!wantToolChange) {
+      Serial.println(">>> spindlestate old=toolchanging new=toolchangewait");
+      doneToolChangeWait = millis() + 5000;  // 5 second wait before unlocking spindle.
+      stateSpindle = stateSpindleToolChangeWait;
+    }
+    break;
+  case stateSpindleToolChangeWait:
+    digitalWrite(pinAirA, 0);  // lock
+    digitalWrite(pinAirB, 0);  // lock
+    if (millis() > doneToolChangeWait) {
+      Serial.println(">>> spindlestate old=toolchangewait new=off");
+      stateSpindle=stateSpindleOff;
+    }
+    break;
+  }
   
   lcd.clear();
   lcd.print("DC: ");
@@ -168,39 +290,16 @@ void loop() {
   lcd.setCursor(0, 1);
   lcd.print(millis() / 1000);
   
-  Serial.println(millis() / 1000);
-  Serial.print("DC Amps: ");
-  Serial.println(dcAmps);
-  Serial.print("Spindle Amps: ");
-  Serial.println(spindleAmps);
-  Serial.print("Vacuum Amps: ");
-  Serial.println(vacAmps);
-  Serial.print("Compressor Amps: ");
-  Serial.println(compressorAmps);
-  
-  Serial.print("CNC: cool=");
-  Serial.print(cncCoolant);
-  Serial.print(", dir=");
-  Serial.print(cncSpindleDirection);
-  Serial.print(", speed=");
-  Serial.print(cncSpindleSpeed);
-  Serial.print(" (avg ");
-  Serial.print(avgSpindleSpeed);
-  Serial.println(")");
-
-  rpm = analogToRpm(avgSpindleSpeed);
   Serial.print("RPM = ");
   Serial.print(rpm);
   Serial.println(" RPM");
 
-  digitalWrite(pinAirA, airState);
-  digitalWrite(pinAirB, airState);
-  digitalWrite(pinAirC, airState);
+  //digitalWrite(pinAirA, airState);
+  //digitalWrite(pinAirB, airState);
+  //digitalWrite(pinAirC, airState);
 
   digitalWrite(pinSpindleOnOff, rpm > 0);
 
-  //Serial.print("Average voltage is ");
-  //Serial.println(1.0*totalVolts/countVolts);
   delay(100);
 
 }
